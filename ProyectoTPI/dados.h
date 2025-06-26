d26a5bb024da6b53f5dc424b0dfa369ba164ce37
Tutumaster23 <cristianifrancisco@gmail.com>
2025-06-25 21:51:16 -0300

    Delete ProyectoTPI directory

--- ProyectoTPI/dados.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-int generarDado6();
-int generarDado12();
-
-void cargarDados6(int dados[], int cantidad);
-void cargarDados12(int dados[], int cantidad);
-
-void mostrarDados(int dados[], int cantidad);
